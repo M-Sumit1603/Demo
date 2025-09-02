c85aeb3df44a23706843d10aa7c6c1801309a79f
M-Sumit1603 <msumit1603@gmail.com>
2025-09-02 10:50:49 +0530

    Update TestRunner.java

--- RestAssuredCucumberAPI/src/test/java/runners/TestRunner.java
+++ RestAssuredCucumberAPI/src/test/java/runners/TestRunner.java
@@ -7,8 +7,9 @@ import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/java/features",
-        glue = {"stepDefinitions"},
-        plugin = {"pretty", "html:target/cucumber-reports.html"},
+        glue = "stepDefinitions",
+        plugin = {"pretty", "json:target/cucumber.json"}, // <-- generate JSON report here
         monochrome = true
 )
-public class TestRunner {}
+public class TestRunner {
+}
